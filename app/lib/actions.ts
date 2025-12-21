76329c57ebc8a353468c127cf139bcf864fb9679
salmaslsbd <salmaslsbd31@gmail.com>
2025-12-21 18:56:30 +0700

    modified

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -125,16 +125,13 @@ export async function updateInvoice(
    DELETE INVOICE
 ======================= */
 export async function deleteInvoice(id: string) {
-  try {
-    await sql`
-      DELETE FROM invoices
-      WHERE id = ${id}
-    `;
-    revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice.' };
-  } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
-  }
+  await sql`
+    DELETE FROM invoices
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
 
 export async function authenticate(
