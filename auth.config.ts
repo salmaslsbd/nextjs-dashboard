c774049c9e398e489152d97a819cf623c66e94a3
salmaslsbd <salmaslsbd31@gmail.com>
2025-12-21 20:02:25 +0700

    modified

--- auth.config.ts
+++ auth.config.ts
@@ -1,21 +1,52 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+import Credentials from 'next-auth/providers/credentials';
+
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
+
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
+        return false; // redirect ke login
+      }
+
+      if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
+
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
+
+  providers: [
+    Credentials({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials) return null;
+
+        if (
+          credentials.email === 'test@test.com' &&
+          credentials.password === '123456'
+        ) {
+          return {
+            id: '1',
+            name: 'Test User',
+            email: 'test@test.com',
+          };
+        }
+
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
